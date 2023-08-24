6c6ab45e38a1282990d225ed5507ce720eb8d51c
Álvaro Braz <alvarobraz83@gmail.com>
2023-08-24 09:44:38 -0300

    implemented interfaces of the home and history

---
+++ src/components/ScreenHeader.tsx
@@ -0,0 +1,15 @@
+import { Center, Heading } from 'native-base';
+
+type Props = {
+  title: string;
+}
+
+export function ScreenHeader({ title }: Props) {
+  return (
+    <Center bg="gray.600" pb={6} pt={16} mb={3}>
+      <Heading color="gray.100" fontSize="xl">
+        {title}
+      </Heading>
+    </Center>
+  );
+}
